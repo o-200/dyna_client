a7955ea4392db8b882a35a289e1cee57d48fb267
Alex Abramov <omgout.200@gmail.com>
2024-12-14 22:01:54 +0700

    first commit

---
+++ lib/dyna_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dyna_client/version"
+
+module DynaClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dyna_client/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DynaClient
+  VERSION = "0.1.0"
+end
